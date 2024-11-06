bfaec7e3f8979832f4e12407cc6235f34d634433
Velasco-A <alb172@correo.ugr.es>
2024-11-06 09:25:14 +0000

    Pending changes exported from your codespace

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+import math
+
+def minimo_comun_multiplo(a, b):
+    return abs(a*b) // math.gcd(a, b)
+
+
+# Ejemplo de uso
+numero1 = int(input("Ingrese el primer número: "))
+numero2 = int(input("Ingrese el segundo número: "))
+minimo_comun_multiplo(numero1, numero2)
+
+print(f"El mínimo común múltiplo de {numero1} y {numero2} es {minimo_comun_multiplo(numero1, numero2)}")
